984f4a579d367d0bf4daebcaac77e8187a2598ef
Glen Maddern <glenmaddern@gmail.com>
2019-09-25 14:35:04 +1000

    no need for ASSETS_HOST

--- fab-server/rewrites.js
+++ fab-server/rewrites.js
@@ -1,10 +1,9 @@
 const cheerio = require('cheerio')
 const manifest = require('./manifest.json')
 const path = require('path')
-console.log({manifest})
 const client_js = path.basename(manifest['client.js'])
 
-const rewriteAssets = ($, ASSETS_HOST) => {
+const rewriteAssets = ($) => {
   $('script').each((i, elem) => {
     const src = $(elem).attr('src')
     if (src && src.startsWith(`/packs/js/client`)) {
